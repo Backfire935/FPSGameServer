6fc53c0fa3d271f709286db433a03ec54799ab2a
Backfire <13866816549@163.com>
2024-03-11 23:40:23 +0800

    LoginServer构建

---
+++ LoginServer/code/AppManager.cpp
@@ -0,0 +1,114 @@
+#include "AppManager.h"
+
+#include <ctime>
+
+#include "AppGlobal.h"
+#include "../../share/ShareFunction.h"
+#include "AppTest.h"
+
+#ifndef ____WIN32_
+#include <unistd.h>
+#endif
+
+namespace app
+{
+	AppManager* __AppManager = nullptr;
+	int temp_time = 0;
+	char printfstr[1000];
+	
+	AppManager::AppManager()
+	{
+	}
+
+	AppManager::~AppManager()
+	{
+		
+	}
+
+	void printInfo()
+	{
+#ifdef ____WIN32_
+		int temptime = (int)time(NULL) - temp_time;
+		if(temptime < 1) return;
+		if(func::__ServerInfo == nullptr) return;
+		
+		int concount = 0;
+		int securitycount = 0;
+		__TcpServer->getSecurityCount(concount, securitycount);//��ӡ��ȫ��������
+		sprintf_s(printfstr, "LoginServer [%d-%d] connect:%d, security:%d", func::__ServerInfo->ID, func::__ServerInfo->Port, concount, securitycount);
+		SetWindowTextA(GetConsoleWindow(),printfstr);
+		
+		temp_time = (int)time(NULL);
+#endif
+	}
+
+	void onUpdate()
+	{
+		if(__TcpServer == nullptr) return;
+		//��ͣ�ļ�����ӣ�����ָ��,��ͣ��Ͷ������
+		__TcpServer->parseCommand();
+		__TcpDB->parseCommand();
+		printInfo();
+	}
+	
+	void AppManager::init()
+	{
+		bool isload = share::InitData();
+		if (!isload) return;
+		if (func::__ServerListInfo.size() < 1) return;
+
+		__TcpServer = net::NewTcpServer();
+
+		//�����µ�����
+		__TcpServer->setOnClientAccept(onClientAccept);
+		//���ð�ȫ����
+		__TcpServer->setOnClientSecureConnect(onClientSecureConnect);
+		//����ʧȥ����
+		__TcpServer->setOnClientDisConnect(onClientDisconnect);
+		//���ó�ʱ����
+		__TcpServer->setOnClientTimeout(onClientTimeout);
+		//�����쳣����
+		__TcpServer->setOnClientExcept(onClientExcept);
+		//���з����
+		__TcpServer->runServer(1);//���������߳�
+
+		auto xml = func::__ServerListInfo[0];
+		 __TcpDB = net::NewTcpClient();
+		 __TcpDB->setOnConnect(onConnect);
+		 __TcpDB->setOnSecure(onSecureConnect);
+		 __TcpDB->setOnDisConnect(onDisconnect);
+		 __TcpDB->setOnExcept(onExcept);
+
+		 __TcpDB->runClient(xml->ID, xml->IP, xml->Port);
+		 __TcpDB->getData()->ID = 0;
+
+		__AppTest = new AppTest();
+
+		//Sleep(5000);
+		//__TcpServer->stopServer();
+		
+		LOG_MSG("CenterServer start ok...%d-%d \n", func
+			::__ServerInfo->ID,func::__ServerInfo->Port);
+
+		while(true)
+		{
+			onUpdate();
+#ifdef ____WIN32_
+			Sleep(5);//2ms����һ��
+#else
+			usleep(5);
+#endif
+
+		}
+	}
+
+	int run()
+	{
+		if(__AppManager == nullptr)
+		{
+			__AppManager = new AppManager();
+			__AppManager->init();
+		}
+		return 0;
+	}
+}
